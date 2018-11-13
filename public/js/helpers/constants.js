5e0b1ba7fd7ee3a647c685ecdb92e790505a6b38
Rafael Cavalcante <rafael.ifrn@gmail.com>
2018-11-13 20:35:51 +0000

    First commit

--- public/js/helpers/constants.js
+++ public/js/helpers/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8888/';
-export const CLIENT_ID = '';
+export const BASE_URL = 'https://jozefinasapp.herokuapp.com/';
+export const CLIENT_ID = '700325fa209943cb98c908e86bbcc343';
 export const STATE_KEY = 'spotify_auth_state';
